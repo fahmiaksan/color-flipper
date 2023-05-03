073a5c293fd9a90d9b0341a4ae05c87a213cccfc
FhmiAksn <fahmiaksannugroho@gmail.com>
2023-05-03 14:27:14 +0700

    make a web color flipper

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const newColourBtnElement = document.getElementById('new-colour-button');
+const currentColourElement = document.getElementById('current-colour');
+const hexValues = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+function getRandomHexValue() {
+    const randomIndexPosition = Math.floor(Math.random() * hexValues.length);
+    const randomHexValue = hexValues[randomIndexPosition];
+    return randomHexValue;
+    // random hex value akan mengembalikan hanya 1 value sesuai yang ada di variable hexValue
+}
+
+function getRandomHexString(stringLength) {
+    let hexString = '';
+    // disini getRandomHexValue akan dilooping sebanyak nilai yang diinput
+    for (let i = 0; i < stringLength; i++) {
+        hexString += getRandomHexValue();
+    }
+    return hexString;
+}
+
+newColourBtnElement.addEventListener('click', function () {
+    const randomHexString = "#" + getRandomHexString(6);
+    document.body.style.setProperty('background-color', randomHexString);
+    currentColourElement.textContent = randomHexString;
+});
